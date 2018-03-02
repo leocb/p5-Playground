ded8f08ad1b4e6fa9f770137e62dcdbc45973546
leocb <leobottaro@gmail.com>
2018-03-02 17:52:32 -0300

    Renamed some projects

--- DX Ball/sketch.js
+++ DX Ball/sketch.js
@@ -7,24 +7,23 @@ function setup() {
 	createCanvas(windowWidth - 45, windowHeight);
 
 	frameRate(60)
-	angleMode(DEGREES)
 
-	paddle = new Paddle(width / 2, height - 50, 150, 20)
-	ball = new Ball(width / 2, height / 2, 15, createVector(2, 2))
+	paddle = new Paddle(width / 2, height / 2, width, 20)
+	ball = new Ball(100 / 2, 100 / 2, 7, createVector(10, 10))
 }
 
 function draw() {
-	background(255)
+	//background(255)
 	let dt = 100 / frameRate()
 
 	paddle.moveTo(Math.min(Math.max(paddle.halfWidth, mouseX), width - paddle.halfWidth))
+	ball.update(dt)
 	ball.checkBounds(0, 0, width, height)
 	ball.checkPaddle(paddle)
-	ball.update(dt)
 	ball.draw()
 	paddle.draw()
-	text(ball.velVector.x, 100, 100)
-	text(ball.velVector.y, 100, 120)
+	// text(ball.pos.x, 100, 100)
+	// text(ball.pos.y, 100, 120)
 }
 
 class Ball {
@@ -33,7 +32,10 @@ class Ball {
 		this.velVector = velocityVector
 		this.accVector = createVector(0, 0)
 		this.radius = radius
+		this.diameter = this.radius * 2
 		this.prev = this.pos
+		this.vMax = 10
+
 	}
 
 	applyForce(force = createVector(0, 0)) {
@@ -50,43 +52,81 @@ class Ball {
 	}
 
 	checkPaddle(paddle = new Paddle) {
-		let intersectPoint = intersect(this.prev.x, this.prev.y, this.pos.x, this.pos.y, paddle.left, paddle.y, paddle.right, paddle.y)
-		console.log(this.prev.x, this.prev.y, this.pos.x, this.pos.y, paddle.left, paddle.y, paddle.right, paddle.y, intersectPoint);
+		let intersectPoint = intersect(this.prev.x, this.prev.y + this.radius, this.pos.x, this.pos.y + this.radius, paddle.left, paddle.y, paddle.right, paddle.y)
+		if (intersectPoint) {
+
+			// calculate how much remains of the path
+			let remaingVector = this.pos.copy()
+			remaingVector.y += this.radius
+			remaingVector.sub(intersectPoint).add(intersectPoint)
+			console.log(remaingVector.mag())
+			console.log(remaingVector.heading());
+
+			//remaingVector.rotate(-remaingVector.copy().normalize().heading())
+			console.log(remaingVector.mag())
+			console.log(this.velVector.mag())
+
+			fill(0, 255, 0, 120)
+			ellipse(intersectPoint.x, intersectPoint.y, 40)
+			stroke(255, 0, 0)
+			line(intersectPoint.x, intersectPoint.y, this.prev.x, this.prev.y + this.radius)
+			stroke(0, 255, 0)
+			line(intersectPoint.x, intersectPoint.y, remaingVector.x, remaingVector.y)
+
+			this.velVector = p5.Vector.fromAngle(radians(map(intersectPoint.x, paddle.left, paddle.right, 210, 330))).mult(Math.min(this.velVector.mag() * 1.02, this.vMax))
+			// this.pos.add(p5.Vector.mult(this.velVector, deltaTime))
+			//this.pos = this.velVector.add()
+			console.log(remaingVector);
+			//this.pos.add(remaingVector)
+			this.velVector.mult(0)
+			//this.pos.y = (intersectPoint.y - (this.pos.y + this.diameter)) + intersectPoint.y
+
+
+			stroke(0, 0, 255)
+
+			line(intersectPoint.x, intersectPoint.y, this.pos.x, this.pos.y + this.radius)
+		}
+		intersectPoint = intersect(this.prev.x, this.prev.y, this.pos.x, this.pos.y, paddle.right, paddle.y, paddle.right, paddle.bottom)
 		if (intersectPoint) {
 			this.pos.y = intersectPoint.y - this.pos.y + intersectPoint.y
-			let mag = this.velVector.mag()
-			this.velVector = p5.Vector.fromAngle(radians(map(this.pos.x, paddle.left, paddle.right, 210, 330))).mult(Math.min(mag * 1.2, 7))
+			this.pos.x = intersectPoint.x - this.pos.x + intersectPoint.x
+			this.velVector.mult(-1)
+		}
+		intersectPoint = intersect(this.prev.x, this.prev.y, this.pos.x, this.pos.y, paddle.left, paddle.y, paddle.left, paddle.bottom)
+		if (intersectPoint) {
+			this.pos.y = intersectPoint.y - this.pos.y + intersectPoint.y
+			this.pos.x = intersectPoint.x - this.pos.x + intersectPoint.x
+			this.velVector.mult(-1)
 		}
 	}
 
-	checkBounds(boundCornerX, boundCornerY, boundWidth, boundHeight) {
-		if (this.pos.x < boundCornerX) {
-			this.pos.x += boundCornerX - (this.pos.x)
+	checkBounds(left, top, right, bottom) {
+		if (this.pos.x < left) {
+			this.pos.x = left - this.pos.x + left
 			this.velVector.x *= -1
 		}
-		if (this.pos.x > boundCornerX + boundWidth) {
-			this.pos.x += (boundCornerX + boundWidth) - (this.pos.x)
+		if (this.pos.x > right) {
+			this.pos.x = right - this.pos.x + right
 			this.velVector.x *= -1
 		}
-		if (this.pos.y < boundCornerY) {
-			this.pos.y += boundCornerY - (this.pos.y)
+		if (this.pos.y < top) {
+			this.pos.y = top - this.pos.y + top
 			this.velVector.y *= -1
 		}
-		if (this.pos.y > boundCornerY + boundHeight) {
-			// this.pos.y += (boundCornerY + boundHeight) - (this.pos.y)
-			// this.velVector.y *= -1
-			this.velVector.mult(0)
+		if (this.pos.y > bottom) {
+			//this.velVector.mult(0)
 		}
 	}
 
 	draw() {
 		ellipseMode(CENTER)
+		stroke(0)
+		line(this.pos.x, this.pos.y, this.prev.x, this.prev.y)
+
 		noStroke()
 		fill(0)
-		ellipse(this.pos.x, this.pos.y, this.radius)
-		fill(200, 100, 100)
-		ellipse(this.prev.x, this.prev.y, this.radius)
 
+		ellipse(this.pos.x, this.pos.y, this.diameter)
 	}
 }
 
@@ -116,17 +156,27 @@ class Brick {
 
 // line intercept math by Paul Bourke http://paulbourke.net/geometry/pointlineplane/
 function intersect(x1, y1, x2, y2, x3, y3, x4, y4) {
+
 	if ((x1 === x2 && y1 === y2) || (x3 === x4 && y3 === y4)) { // one of the lines is length 0
 		return false
 	}
-	let denominator = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
-	let ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator
-	let ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator
-	let x = x1 + ua * (x2 - x1)
-	let y = y1 + ua * (y2 - y1)
-	if (x === -Infinity || x === Infinity || y === -Infinity || y === Infinity) {
+
+	denominator = ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+
+	if (denominator === 0) {
 		return false
 	}
+
+	let ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator
+	let ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator
+
+	if (ua < 0 || ua > 1 || ub < 0 || ub > 1) {
+		return false
+	}
+
+	let x = x1 + ua * (x2 - x1)
+	let y = y1 + ua * (y2 - y1)
+
 	return createVector(x, y)
 }
 
